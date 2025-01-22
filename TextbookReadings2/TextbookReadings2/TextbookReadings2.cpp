aa9ab7ef379beddd4fc9171863fb887be73a8e42
GhostDaGhost <53021768+GhostDaGhost@users.noreply.github.com>
2025-01-21 23:37:03 -0800

    Finish up reading

--- TextbookReadings2/TextbookReadings2/TextbookReadings2.cpp
+++ TextbookReadings2/TextbookReadings2/TextbookReadings2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdarg.h>
+#include <cstring>
 
 using namespace std;
 
@@ -64,6 +65,82 @@ static const double Sum(int n, double val ...) {
 	// END ARGUMENT RUNTIME AND RETURN SUM
 	va_end(args);
 	return sumOfArguments;
+}
+
+// SHORTCUT FUNCTION FOR NAMES (REDUCES CODE :))
+static void ProcessNames(char* names[], const int size, string type = "Read") {
+	for (int i = 0; i < size; i++) {
+		if (type == "Read") {
+            cout << names[i] << endl;
+		} else if (type == "Write") {
+            char temp[100];
+
+            // ASK FOR USER INPUT
+            cout << "Enter name " << i + 1 << ": ";
+            cin >> temp;
+
+            // ALLOCATE MEMORY
+            names[i] = new char[strlen(temp) + 1];
+            strcpy_s(names[i], sizeof(names[i]), temp);
+		}
+	}
+}
+
+// READ NAMES
+static void ReadNames(char* names[], const int size) {
+    ProcessNames(names, size, "Read");
+}
+
+// WRITE NAMES
+static void WriteNames(char* names[], const int size) {
+	ProcessNames(names, size, "Write");
+}
+
+// BUBBLE SORT
+static void BubbleSort(char* names[], const int size) {
+    bool sorted = true;
+    while (sorted) {
+        sorted = false;
+        for (int i = 0; i < size - 1; i++) {
+            if (strcmp(names[i], names[i + 1]) > 0) {
+                char* temp = names[i];
+                names[i] = names[i + 1];
+                names[i + 1] = temp;
+                sorted = true;
+            }
+        }
+    }
+}
+
+// 5.28
+static char* ReverseString(char* str) {
+    size_t len = strlen(str);
+    char* result = new char[len + 1];
+    char* start = result;
+
+    char* end = str + len - 1;
+    while (end >= str) {
+        *start++ = *end--;
+    }
+
+    *start = '\0';
+    return result;
+}
+
+// BUBBLE SORT 5.29
+static void RewrittenBubbleSort(char* names[], const int size, int (*compare)(const char*, const char*)) {
+    bool sorted = true;
+    while (sorted) {
+        sorted = false;
+        for (int i = 0; i < size - 1; i++) {
+            if (compare(names[i], names[i + 1]) > 0) {
+                char* temp = names[i];
+                names[i] = names[i + 1];
+                names[i + 1] = temp;
+                sorted = true;
+            }
+        }
+    }
 }
 
 // ON INITIALIZATION
