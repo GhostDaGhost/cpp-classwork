6d6103abbec894922afb68f01a4126059d0e4693
GhostDaGhost <53021768+GhostDaGhost@users.noreply.github.com>
2025-01-31 22:09:35 -0800

    Working step 1

--- Shapes/Shapes/Shapes.cpp
+++ Shapes/Shapes/Shapes.cpp
@@ -5,6 +5,10 @@
 using namespace cv;
 using namespace std;
 
+// DEFINES
+constexpr auto WINDOW_WIDTH = 500;;
+constexpr auto WINDOW_HEIGHT = 500;;
+
 // DEFINE COLORS
 Scalar black(0, 0, 0);
 Scalar red(0, 0, 255);
@@ -12,21 +16,106 @@ Scalar green(0, 255, 0);
 Scalar blue(255, 0, 0);
 Scalar purple(255, 0, 255);
 
+// DEFINE SHAPES
+enum ShapeType {
+	CIRCLE,
+	ELLIPSE,
+	RECTANGLE,
+	TRIANGLE,
+	LINE
+};
+
+// CIRCLE DRAWER
+void drawCircle(Mat image) {
+	circle(image, Point(250, 100), 50, red, -1);
+}
+
+// ELLIPSE DRAWER
+void drawEllipse(Mat image) {
+	ellipse(image, Point(250, 400), Size(100, 50), 0, 0, 360, purple, -1);
+}
+
+// RECTANGLE DRAWER
+void drawRectangle(Mat image) {
+	rectangle(image, Point(50, 50), Point(150, 150), green, -1);
+}
+
+// TRIANGLE DRAWER
+void drawTriangle(Mat image) {
+	Point triangle[1][3] = { {Point(350, 50), Point(300, 150), Point(400, 150)} };
+
+	const Point* ppt[1] = { triangle[0] };
+	const int npt[] = { 3 };
+
+	fillPoly(image, ppt, npt, 1, blue);
+}
+
+// LINE DRAWER
+void drawLine(Mat image) {
+	line(image, Point(50, 300), Point(450, 300), black, 2);
+}
+
+// GET SHAPE TYPE ENUM BASED OFF ARGUMENT
+ShapeType getEnumShapeType(const string& argument) {
+	if (argument == "circle") {
+		return CIRCLE;
+	} else if (argument == "ellipse") {
+		return ELLIPSE;
+	} else if (argument == "rectangle") {
+		return RECTANGLE;
+	} else if (argument == "triangle") {
+		return TRIANGLE;
+	} else if (argument == "line") {
+		return LINE;
+	}
+	return CIRCLE;
+}
+
+// TRIGGER CORRECT DRAW FUNCTION BASED OFF ENUM
+void drawShapeWithEnum(Mat image, const ShapeType& shapeTypeEnum) {
+	switch (shapeTypeEnum) {
+		case CIRCLE:
+			drawCircle(image);
+			break;
+		case ELLIPSE:
+			drawEllipse(image);
+			break;
+		case RECTANGLE:
+			drawRectangle(image);	
+			break;
+		case TRIANGLE:
+			drawTriangle(image);
+			break;
+		case LINE:
+			drawLine(image);
+			break;
+		default:
+			cout << "Something went wrong! Could not find enum! Going to draw circle instead." << endl;
+			drawCircle(image);
+			break;
+	}
+}
+
 // ON INITIALIZATION, CAPTURE ARGUMENTS AND START SHAPES
 int main(int argumentCount, char* arguments[]) {
-	const int width = 500, height = 500;
-	Mat image = Mat(height, width, CV_8UC3, Scalar(255, 255, 255));
+	if (argumentCount <= 1) {
+		cout << "Please enter a shape to draw! (circle, ellipse, rectangle, triangle, line)" << endl;
+		return 0;
+	}
 
-	// DEBUG CIRCLE
-	circle(image, cv::Point(250, 100), 50, red, -1);
+	// MAKE WHITE IMAGE AND ITERATE THROUGH ARGUMENTS
+	Mat image = Mat(WINDOW_HEIGHT, WINDOW_WIDTH, CV_8UC3, Scalar(255, 255, 255));
+	for (int i = 0; i < argumentCount; i++) {
+		const string argument = toLowerCase(arguments[i]);
+		const ShapeType shapeTypeEnum = getEnumShapeType(argument);
 
-	// ITERATE THROUGH ARGUMENTS
-	/*for (int i = 0; i < argumentCount; i++) {
-		//
-	}*/
+		// TRIGGER INVOKER WITH ENUM TO START DRAWING SHAPES
+		drawShapeWithEnum(image, shapeTypeEnum);
+		imshow("Shapes", image);
+		waitKey(2000);
+	}
 
-	// DISPLAY IMAGE AND WAIT FOR KEYPRESS TO DESTROY WINDOWS
-	imshow("Shapes", image);
+	// MAKE A LOOP
 	waitKey(0);
 	destroyAllWindows();
 
