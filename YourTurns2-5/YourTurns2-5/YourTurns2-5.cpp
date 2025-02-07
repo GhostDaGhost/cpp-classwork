b8438be47185356737eea39ce010388058ea63db
GhostDaGhost <53021768+GhostDaGhost@users.noreply.github.com>
2025-02-06 23:50:09 -0800

    Half of the Your Turns

--- YourTurns2-5/YourTurns2-5/YourTurns2-5.cpp
+++ YourTurns2-5/YourTurns2-5/YourTurns2-5.cpp
@@ -1,17 +1,85 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
-int main() {
-	ifstream SampleFile("sample.txt");
+struct ListNode {
+    string data;
+    ListNode* next;
+    ListNode(string w) : data(w), next(nullptr) {};
+};
 
-	// ITERATE THROUGH EACH LINE
-	string fileText;
-	for (int i = 9; i >= 0; i--) {
-		cout << "Line " << i << ": " << fileText << endl;
+static int getSizeOfNode(ListNode* myList) {
+    if (myList == NULL) {
+        return 0;
+    }
+    return 1 + getSizeOfNode(myList->next);
+}
+
+static void displayList(ListNode* myList) {
+    if (myList != NULL) {
+        cout << myList->data << " ";
+        displayList(myList->next);
+    }
+}
+
+static void displayReverseList(ListNode* myList) {
+	if (myList != NULL) {
+		displayReverseList(myList->next);
+		cout << myList->data << " ";
+	}
+}
+
+static void insertIntoSortedList(ListNode* &head, string word) {
+    ListNode* newHead = new ListNode(word);
+    if (!head || word < head->data) {
+        newHead->next = head;
+        head = newHead;
+        return;
+    }
+
+    ListNode* currentHead = head;
+    while (currentHead->next && currentHead->next->data < word) {
+        currentHead = currentHead->next;
+    }
+
+    newHead->next = currentHead->next;
+    currentHead->next = newHead;
+}
+
+int main(int argumentCount, char* arguments[]) {
+	if (argumentCount != 2) {
+		cout << "Usage: <filename>" << endl;
+		return 1;
 	}
 
-	// CLOSE FILE WHEN FINISHED
-	SampleFile.close();
+    // OPEN FILE AND CATCH ANY ERRORS
+    ifstream file(arguments[1]);
+    if (!file) {
+        cout << "There was an error opening this file!" << endl;
+    }
+
+    // CREATE EMPTY LIST
+    ListNode* head = nullptr;
+    string wordInList;
+
+    // ITERATE THROUGH FILE'S WORDS
+	cout << "Words in file: ";
+    while (file >> wordInList) {
+        cout << wordInList << " ";
+        insertIntoSortedList(head, wordInList);
+    }
+
+    // CLOSE FILE
+    file.close();
+
+    // SHOW LIST IN ORDER
+    cout << "\nSize of list: " << getSizeOfNode(head) << endl;
+	cout << "List in ALPHABETICAL order: ";
+    displayList(head);
+
+    // SHOW LIST IN REVERSE ORDER
+	cout << "\nList in REVERSE order: ";
+	displayReverseList(head);
 }
